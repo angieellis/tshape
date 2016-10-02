718df614ff1c41d512f89bcf0ef5503e03fc2f7f
JugglerX <rob@jugglerdigital.com>
2016-10-02 15:33:48 -0700

    sec

--- src/tshape/settings/base.py
+++ src/tshape/settings/base.py
@@ -22,7 +22,8 @@ ROOT_DIR = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
 # See https://docs.djangoproject.com/en/1.9/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+# SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = "XXasfsadf"
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
