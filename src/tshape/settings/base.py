dd1d2254e4c2b198453b7b5587065f1bc9f143bc
JugglerX <rob@jugglerdigital.com>
2016-10-02 15:26:37 -0700

    added postgres config for heroku

--- src/tshape/settings/base.py
+++ src/tshape/settings/base.py
@@ -172,3 +172,14 @@ STATICFILES_FINDERS = (
 #     )
     # 'TEST_REQUEST_DEFAULT_FORMAT': 'json'
 # }
+
+
+# Update database configuration with $DATABASE_URL.
+db_from_env = dj_database_url.config(conn_max_age=500)
+DATABASES['default'].update(db_from_env)
+
+# Honor the 'X-Forwarded-Proto' header for request.is_secure()
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
+# Allow all host headers
+ALLOWED_HOSTS = ['*']
